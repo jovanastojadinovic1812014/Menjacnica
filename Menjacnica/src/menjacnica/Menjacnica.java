e62fe668b149093d731cc3d784f08301d9803fd3
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-24 17:34:25 +0100

    Napravljena klasa Menjacnica koja implementira metode interfejsa MenjacnicaInterfejs. Metode su sa "praznim" telom.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,24 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(String nazivValute, Kurs kurs) {
+
+	}
+
+	@Override
+	public void obrisiKurs(String nazivValute, Kurs kurs) {
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(String nazivValute, GregorianCalendar datum) {
+		return null;
+	}
+
+}
