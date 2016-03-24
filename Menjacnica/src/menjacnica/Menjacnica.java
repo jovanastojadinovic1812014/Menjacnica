cda35bc3bbc896e05cc87aeda65947ebff7f7887
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-24 18:01:54 +0100

    Implementirane metode u klasi Menjacnica na master grani.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,23 +1,54 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
+
 	@Override
 	public void dodajKurs(String nazivValute, Kurs kurs) {
-
+		if (nazivValute == null || kurs == null) {
+			return;
+		}
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getNazivValute().equals(nazivValute)) {
+				valute.get(i).getKursevi().add(kurs);
+			}
+		}
 	}
 
 	@Override
 	public void obrisiKurs(String nazivValute, Kurs kurs) {
-
+		if (nazivValute == null || kurs == null) {
+			return;
+		}
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getNazivValute().equals(nazivValute)) {
+				valute.get(i).getKursevi().remove(kurs);
+			}
+		}
 	}
 
 	@Override
 	public Kurs pronadjiKurs(String nazivValute, GregorianCalendar datum) {
+		if (nazivValute == null || datum == null) {
+			return null;
+		}
+		for (int i = 0; i < valute.size(); i++) {
+
+			if (valute.get(i).getNazivValute().equals(nazivValute)) {
+
+				for (int j = 0; j < valute.get(i).getKursevi().size(); j++) {
+					if (valute.get(i).getKursevi().get(j).getDatum().equals(datum)) {
+						return valute.get(i).getKursevi().get(j);
+					}
+				}
+			}
+		}
 		return null;
 	}
 
