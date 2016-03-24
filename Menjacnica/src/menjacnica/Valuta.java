85c57b589c64a11a25b0ea3124dd36771cde4d4c
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-24 16:49:40 +0100

    Uvedene metode toString, equals i hashCode u klasama Valuta i Kurs.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -20,4 +20,40 @@ public class Valuta {
 		this.skraceniNazivValute = skraceniNazivValute;
 	}
 
+	@Override
+	public String toString() {
+		return "Valuta [nazivValute=" + nazivValute + ", skraceniNazivValute=" + skraceniNazivValute + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		result = prime * result + ((skraceniNazivValute == null) ? 0 : skraceniNazivValute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (skraceniNazivValute == null) {
+			if (other.skraceniNazivValute != null)
+				return false;
+		} else if (!skraceniNazivValute.equals(other.skraceniNazivValute))
+			return false;
+		return true;
+	}
+
 }
